27722054bb7b71577db2a55c695ac9f16f03c1bc
hand <enitsed@mhand.net>
2026-02-20 17:45:17 +0900

    build: add kotlin-test base dependency to Ktor to resolve IDE inspection warning

--- ktor-api-server/build.gradle.kts
+++ ktor-api-server/build.gradle.kts
@@ -34,6 +34,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.3")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.3")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.3")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:1.9.21")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:1.9.21")
 }
 
